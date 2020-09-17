8b0115847265765cc9cd35b4c200dbc06f166bd9
ethan0401 <71141660+ethan0401@users.noreply.github.com>
2020-09-17 01:30:36 +0000

    i made this cool monkey chillin like a villin

--- index.java
+++ index.java
@@ -3,7 +3,7 @@ void setup() {
 }
 
 void draw() {
-  background(255);
+  background(0)   
   ellipseMode(CENTER);
   ellipse(mouseX, mouseY, 1, 1);
   println(mouseX, mouseY);
@@ -11,14 +11,44 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+  fill(130, 75, 5);
+  ellipse(90, 110, 50, 50)
   
- 
-  
-  
- 
-
-  
- 
-	
-
+  fill(130, 75, 5);
+  ellipse(310, 110, 50, 50);
+  fill(255, 200, 200);
+  ellipse(100, 115, 30, 30);
+  ellipse(300, 115, 30, 30);
+  fill(130, 75, 5);
+  ellipse(200, 330, 500, 250)
+  fill(130, 75, 5);
+  ellipse(200, 150, 200, 200);
+  fill(230, 40, 40);
+  ellipse(200, 190, 100, 39);
+  fill(235, 45, 45);
+  rect(200, 180, 100, -20); 
+  fill(255);
+  ellipse(170, 110, 35, 35);
+  fill(255);
+  ellipse(230, 110, 35, 35);
+  fill(0, 200, 200);
+  ellipse(170, 110, 25, 25);
+  fill(0, 200, 200);
+  ellipse(230, 110, 25, 25);
+  fill(0);
+  ellipse(190, 145, 10, 10);
+  fill(0);
+  ellipse(170, 110, 10, 10);
+	fill(0);
+  ellipse(235, 115, 10, 10);
+  ellipse(210, 145, 10, 10);
+  fill(255, 200, 200);
+  ellipse(110, 350, 30, 30);
+  ellipse(290, 350, 30, 30);
+  fill(205, 110, 110);
+  ellipse(110, 350, 15, 15);
+  ellipse(290, 350, 15, 15);
 }
+
+
+
